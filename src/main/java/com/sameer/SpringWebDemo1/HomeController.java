e42a202e7678a056267bc3e4db33ba0c1cee6131
admin <sameerburli10@gmail.com>
2024-11-21 20:24:37 +0530

    Pushing the error code

--- src/main/java/com/sameer/SpringWebDemo1/HomeController.java
+++ src/main/java/com/sameer/SpringWebDemo1/HomeController.java
@@ -1,10 +1,11 @@
 package com.sameer.SpringWebDemo1;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+
+
 
 @Controller
 public class HomeController {
@@ -12,19 +13,16 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String add(HttpServletRequest req, HttpSession session){
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
         int result = num1 + num2;
-        session.setAttribute("num1", num1);
-        session.setAttribute("num2", num2);
-        session.setAttribute("result", result);
-        return "result.jsp";
-
+        model.addAttribute("num1", num1);
+        model.addAttribute("num2", num2);
+        model.addAttribute("result", result);
+        return "result";
     }
 
 }
